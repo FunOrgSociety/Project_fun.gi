65dab5dfed1ee8160534221bd9ebc61ef5427122
Antonio Radolović <anradol@unipu.hr>
2019-01-27 22:42:23 +0100

    data grid

--- WindowsFormsApp2/PretragaForm.cs
+++ WindowsFormsApp2/PretragaForm.cs
@@ -25,6 +25,35 @@ namespace WindowsFormsApp2
             populateCombo1();
             populateCombo2();
             populateCombo3();
+
+            DataTable dt = new DataTable();
+
+            dt.Columns.Add("Naziv");
+            dt.Columns.Add("Boja");
+            dt.Columns.Add("Mjesto branja");
+            dt.Columns.Add("Vrijeme");
+            dt.Columns.Add("Strucak");
+            dt.Columns.Add("Klobuk");
+
+            gljiva = GljivaClass.dohvati();
+
+            foreach (var a in gljiva)
+            {
+                var row = dt.NewRow();
+
+                row["Naziv"] = Convert.ToString(a.naziv);
+                row["Boja"] = Convert.ToString(a.Boja);
+                row["Mjesto branja"] = Convert.ToString(a.Mjesto_branja);
+                row["Vrijeme"] = Convert.ToString(a.Vrijeme_branja);
+                row["Strucak"] = Convert.ToString(a.Strucak);
+                row["Klobuk"] = Convert.ToString(a.Klobuk);
+
+                dt.Rows.Add(a.naziv, a.Boja, a.Mjesto_branja, a.Vrijeme_branja, a.Strucak, a.Klobuk);
+
+            }
+            dataGridView1.DataSource = dt;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
         }
 
         private void pictureBox3_Click(object sender, EventArgs e)
